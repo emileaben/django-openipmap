730908b0176e163cf705cd685b579aa4d210b2ae
Emile Aben <emileaben@gmail.com>
2014-12-05 10:35:09 +0100

    now last_updated is going to be updated as well

--- management/commands/expireiphostnamemapping.py
+++ management/commands/expireiphostnamemapping.py
@@ -10,13 +10,14 @@ UPDATE_INTERVAL=86400 # one day
 def total_seconds( td ):
    return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
 
-def check_and_expire( ip, ipm_list, now ):
+def check_and_expire( ip, ipm_list ):
    ''' given an IP address and a list of IPMeta objects for that IP, expire all info in db that is not up to date '''
    print "%s %s" %  (ip, len( ipm_list ) )
    if len( ipm_list ) > 1:
       ### pick one, rest should have been invalidated anyways
       for ipm in ipm_list[1:]:
-         ipm.invalidated=now
+         ipm.invalidated=datetime.now(pytz.utc)
+         ipm.last_updated=datetime.now(pytz.utc)
          ipm.save()
    ipm = ipm_list[0] # this is the chosen ipmeta object to validate against
    now_dnshost = None
@@ -26,7 +27,8 @@ def check_and_expire( ip, ipm_list, now ):
       now_dnsloc = do_dns_loc_lookup( now_dnshost )
    if ipm.hostname != now_dnshost or ipm.dnsloc != now_dnsloc:
       # invalidate old and create new
-      ipm.invalidated = now
+      ipm.invalidated = datetime.now(pytz.utc)
+      ipm.last_updated=datetime.now(pytz.utc)
       ipm.save()
       # clone didn't work ( https://docs.djangoproject.com/en/1.7/topics/db/queries/#copying-model-instances )
       # so doing a new object for now
@@ -35,6 +37,9 @@ def check_and_expire( ip, ipm_list, now ):
          ip= ipm.ip
       )
       ipm_new.save() ## this will do another DNS lookup (which should be in cache anyways)
+   else: #save the last time this info was checked
+      ipm.last_updated=datetime.now(pytz.utc)
+      ipm.save()
 
 class Command(BaseCommand):
    args = ''
@@ -52,38 +57,8 @@ class Command(BaseCommand):
                continue
             else:
                ## check validity for this IP and update if needed
-               check_and_expire( current_ip , ipm_list, now )
+               check_and_expire( current_ip , ipm_list )
                # reset
                ipm_list = [ipm]
                current_ip = ipm.ip
-         check_and_expire( current_ip, ipm_list, now )      
-         
-'''
-            if ipm.invalidated == None and ipm.hostname == None or total_seconds(now - ipm.last_updated) > UPDATE_INTERVAL: ###TODO check last-updated against current time:
-                  now_dnshost = None
-                  now_dnsloc = None
-                  now_dnshost = do_dns_host_lookup( ipm.ip )
-                  try:
-                     now_dnshost = now_dnshost.rstrip('.')
-                  except: pass ## will fail if dnshost=none
-                  if now_dnshost != None:
-                     now_dnsloc = do_dns_loc_lookup( now_dnshost )
-                  if ipm.hostname != now_dnshost or ipm.dnsloc != now_dnsloc:
-                     # invalidate old and create new
-                     ipm.invalidated = now
-                     ipm.save()
-                     # clone didn't work ( https://docs.djangoproject.com/en/1.7/topics/db/queries/#copying-model-instances )
-                     # so doing a new object for now
-                     print "creating new! for %s/%s" % ( ipm.ip , ipm.hostname )
-                     ipm_new = IPMeta(
-                        ip= ipm.ip
-                     )
-                     ipm_new.save() ## this will do another DNS lookup (which should be in cache anyways)
-                     # this didnt work:
-                     #ipm.invalidated = None
-                     #ipm.hostname = now_dnshost
-                     #ipm.dnsloc   = now_dnsloc
-                     #ipm.save()
-            #TODO: case of retry/expiry due to being too old
-            # retry/expire after a week?
-'''
+         check_and_expire( current_ip, ipm_list )      
